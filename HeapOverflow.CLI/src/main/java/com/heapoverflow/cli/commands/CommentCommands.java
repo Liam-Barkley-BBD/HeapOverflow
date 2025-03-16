213d418e3c98c5e1094f8405715dfdb1bb7f67ea
bbdnet3218 <michael.chinyama@bbd.co.za>
2025-03-16 19:29:34 +0200

    making commands easier to use

--- HeapOverflow.CLI/src/main/java/com/heapoverflow/cli/commands/CommentCommands.java
+++ HeapOverflow.CLI/src/main/java/com/heapoverflow/cli/commands/CommentCommands.java
@@ -13,189 +13,142 @@ import com.heapoverflow.cli.utils.TextUtils;
 
 @ShellComponent
 public class CommentCommands {
-    @ShellMethod(key = "comments", value = "Get comments from the system")
-    public String comments(
-        @ShellOption(value = "page", help = "Page number", defaultValue = "0") int page,
+    
+    @ShellMethod(key = "comment", value = "Manage comments in the system")
+    public String comment(
+        @ShellOption(value = "list", help = "List replies", defaultValue = "false") boolean list,
+        @ShellOption(value = "get", help = "Get a specific reply", defaultValue = "false") boolean get,
+        @ShellOption(value = "post", help = "Post a new reply", defaultValue = "false") boolean post,
+        @ShellOption(value = "edit", help = "Edit a reply", defaultValue = "false") boolean edit,
+        @ShellOption(value = "delete", help = "Delete a reply", defaultValue = "false") boolean delete,
+        @ShellOption(value = "id", help = "Comment ID", defaultValue = "") String id,
+        @ShellOption(value = "content", help = "Comment content", defaultValue = "") String content,
+        @ShellOption(value = "threadId", help = "Thread ID", defaultValue = "") String threadId,
+        @ShellOption(value = "page", help = "Page number", defaultValue = "1") int page,
         @ShellOption(value = "size", help = "Page size", defaultValue = "5") int size
     ) {
-        page = page - 1;
+        if (!EnvUtils.doesKeyExist(EnvConstants.JWT_TOKEN)) {
+            return "You are not logged in. Please log in!";
+        } else if (list) {
+            return getAllComments(page, size);
+        } else if (get) {
+            return getComment(id);
+        } else if (post) {
+            return postComment(content, threadId);
+        } else if (edit) {
+            return editComment(id, content);
+        } else if (delete) {
+            return deleteComment(id);
+        } else {
+            return "Invalid command. Use --list, --get --id {id}, --post --content \"text\" --threadId {id}, --edit --id {id} --content \"text\", or --delete --id {id}.";
+        }
+    }
 
-        if(!EnvUtils.doesKeyExist(EnvConstants.JWT_TOKEN)){
-            return "You are not logged, please login!";
+    private String getAllComments(int page, int size) {
+        try {
+            JsonNode jsonResponse = CommentsServices.getComments(Math.max(0, page - 1), size);
+            JsonNode contentArray = jsonResponse.path("content");
+
+            if (!contentArray.isArray() || contentArray.isEmpty()) {
+                return "No comments found.";
+            } else{
+                TableModelBuilder<String> modelBuilder = buildReplyTable(contentArray);
+
+                int totalThreads = jsonResponse.path("totalElements").asInt(0);
+                int totalPages = jsonResponse.path("totalPages").asInt(1);
+                int currentPage = jsonResponse.path("number").asInt(0) + 1;
+                boolean isLastPage = jsonResponse.path("last").asBoolean();
+                
+                return TextUtils.renderTable(modelBuilder.build()) +
+                    String.format("\nPage %d of %d | Total Comments: %d %s", currentPage, totalPages, totalThreads, isLastPage ? "(Last Page)" : "");
+            }
+        } catch (Exception e) {
+            return "Error retrieving comments: " + e.getMessage();
+        }
+    }
+
+    private String getComment(String id) {
+        if (id.isEmpty()) {
+            return "The id must be specified like: 'comment get --id {id_value}'";
         } else{
             try {
-
-                JsonNode jsonResponse = CommentsServices.getComments(page, size);
-                JsonNode contentArray = jsonResponse.path("content");
-
-                if (!contentArray.isArray() || contentArray.isEmpty()) {
-                    return "No comments found.";
-                } else {
-
-                    int totalThreads = jsonResponse.path("totalElements").asInt(0);
-                    int totalPages = jsonResponse.path("totalPages").asInt(1);
-                    int currentPage = jsonResponse.path("number").asInt(0) + 1;
-                    boolean isLastPage = jsonResponse.path("last").asBoolean();
-
-                    TableModelBuilder<String> modelBuilder = new TableModelBuilder<>();
-                    modelBuilder.addRow().addValue("ID").addValue("Content")
-                        .addValue("GID").addValue("User").addValue("Email")
-                        .addValue("Created At").addValue("Upvotes").addValue("ThreadId");
-
-                    for (JsonNode comment : contentArray) {
-                        JsonNode userNode = comment.path("user");
-
-                        modelBuilder.addRow()
-                                .addValue(comment.path("id").asText("N/A"))
-                                .addValue(comment.path("content").asText("N/A"))
-                                .addValue(userNode.path("id").asText("N/A"))
-                                .addValue(userNode.path("username").asText("N/A"))
-                                .addValue(userNode.path("email").asText("N/A"))
-                                .addValue(comment.path("createdAt").asText("N/A"))
-                                .addValue(comment.path("commentUpvotesCount").asText("N/A"))
-                                .addValue(comment.path("threadId").asText("N/A"));
-                    }
-
-                    return TextUtils.renderTable(modelBuilder.build()) + 
-                            String.format("\nPage %d of %d | Total Comments: %d %s",
-                            currentPage, totalPages, totalThreads, isLastPage ? "(Last Page)" : "");
-
-                }
+                JsonNode reply = CommentsServices.getCommentById(id);
+                return TextUtils.renderTable(buildReplyTable(reply).build());
             } catch (Exception e) {
-                return "Error retrieving comments: " + e.getMessage();
+                return "Error retrieving comment: " + e.getMessage();
             }
         }
     }
 
-    @ShellMethod(key = "comment", value = "Get a specific comment in the system")
-    public String comment(
-        @ShellOption(value = "id", help = "the id you wish to find out more details about", defaultValue = "") String id
-    ) {
-        if(!EnvUtils.doesKeyExist(EnvConstants.JWT_TOKEN)){
-            return "You are not logged, please login!";
-        } else if(id.equals("")){
-            return "the id must be specified like: \"comment --id {id_value}\"";
+    private String postComment(String content, String threadId) {
+        if (threadId.isEmpty()) {
+            return "The threadId must be specified like: 'comment post --content \"{content_value}\" --threadId {threadId_value}'";
         } else{
-            try{
-
-                JsonNode comment = CommentsServices.getCommentById(id);
-
-                TableModelBuilder<String> modelBuilder = new TableModelBuilder<>();
-                    modelBuilder.addRow().addValue("ID").addValue("Content")
-                        .addValue("GID").addValue("User").addValue("Email")
-                        .addValue("Created At").addValue("Upvotes").addValue("ThreadId");
-
-                JsonNode userNode = comment.path("user");
-
-                modelBuilder.addRow()
-                        .addValue(comment.path("id").asText("N/A"))
-                        .addValue(comment.path("content").asText("N/A"))
-                        .addValue(userNode.path("id").asText("N/A"))
-                        .addValue(userNode.path("username").asText("N/A"))
-                        .addValue(userNode.path("email").asText("N/A"))
-                        .addValue(comment.path("createdAt").asText("N/A"))
-                        .addValue(comment.path("commentUpvotesCount").asText("N/A"))
-                        .addValue(comment.path("threadId").asText("N/A"));
-
-                return TextUtils.renderTable(modelBuilder.build());
-            }catch(Exception error){
-                return error.getMessage();
+            try {
+                JsonNode reply = CommentsServices.postComment(content, threadId);
+                return TextUtils.renderTable(buildReplyTable(reply).build());
+            } catch (Exception e) {
+                return "Error posting comment: " + e.getMessage();
             }
         }
     }
 
-    @ShellMethod(key = "post-comment", value = "Post a new comment")
-    public String postReply(
-        @ShellOption(value = "content", help = "The content you wish to add in your reply", defaultValue = "") String content,
-        @ShellOption(value = "threadId", help = "The id of the thread you wish to comment on", defaultValue = "") String threadId
-    ) {
-        if(!EnvUtils.doesKeyExist(EnvConstants.JWT_TOKEN)){
-            return "You are not logged, please login!";
-        } else if(threadId.equals("")){
-            return "the threadId must be specified like: \"post-comment --content \"{content_value}\" --threadId {threadId_value}\"";
+    private String editComment(String id, String content) {
+        if (id.isEmpty()) {
+            return "The id must be specified like: 'comment edit --id {id_value} --content \"{content_value}\"'";
         } else{
-            try{
-
-                JsonNode yourComment = CommentsServices.postComment(content, threadId);
-
-                TableModelBuilder<String> modelBuilder = new TableModelBuilder<>();
-                    modelBuilder.addRow().addValue("ID").addValue("Content")
-                        .addValue("GID").addValue("User").addValue("Email")
-                        .addValue("Created At").addValue("Upvotes").addValue("ThreadId");
-
-                JsonNode userNode = yourComment.path("user");
-
-                modelBuilder.addRow()
-                        .addValue(yourComment.path("id").asText("N/A"))
-                        .addValue(yourComment.path("content").asText("N/A"))
-                        .addValue(userNode.path("id").asText("N/A"))
-                        .addValue(userNode.path("username").asText("N/A"))
-                        .addValue(userNode.path("email").asText("N/A"))
-                        .addValue(yourComment.path("createdAt").asText("N/A"))
-                        .addValue(yourComment.path("commentUpvotesCount").asText("N/A"))
-                        .addValue(yourComment.path("threadId").asText("N/A"));
-
-                return TextUtils.renderTable(modelBuilder.build());
-            }catch(Exception error){
-                return error.getMessage();
+            try {
+                JsonNode reply = CommentsServices.patchComment(content, id);
+                return TextUtils.renderTable(buildReplyTable(reply).build());
+            } catch (Exception e) {
+                return "Error editing comment: " + e.getMessage();
             }
         }
     }
 
-    @ShellMethod(key = "edit-comment", value = "Change the content of a comment to something else")
-    public String editComment(
-        @ShellOption(value = "id", help = "The id of the comment you wish to edit", defaultValue = "") String id,
-        @ShellOption(value = "content", help = "The content you wish to change to in your comment", defaultValue = "") String content
-    ) {
-        if(!EnvUtils.doesKeyExist(EnvConstants.JWT_TOKEN)){
-            return "You are not logged, please login!";
-        } else if(id.equals("")){
-            return "the id must be specified like: \"edit-comment --id {id_value} --content \"{content_value}\"\"";
+    private String deleteComment(String id) {
+        if (id.isEmpty()) {
+            return "The id must be specified like: 'comment delete --id {id_value}'";
         } else{
-            try{
-
-                JsonNode yourComment = CommentsServices.patchComment(content, id);
-
-                TableModelBuilder<String> modelBuilder = new TableModelBuilder<>();
-                    modelBuilder.addRow().addValue("ID").addValue("Content")
-                        .addValue("GID").addValue("User").addValue("Email")
-                        .addValue("Created At").addValue("Upvotes").addValue("ThreadId");
-
-                JsonNode userNode = yourComment.path("user");
-
-                modelBuilder.addRow()
-                        .addValue(yourComment.path("id").asText("N/A"))
-                        .addValue(yourComment.path("content").asText("N/A"))
-                        .addValue(userNode.path("id").asText("N/A"))
-                        .addValue(userNode.path("username").asText("N/A"))
-                        .addValue(userNode.path("email").asText("N/A"))
-                        .addValue(yourComment.path("createdAt").asText("N/A"))
-                        .addValue(yourComment.path("commentUpvotesCount").asText("N/A"))
-                        .addValue(yourComment.path("threadId").asText("N/A"));
-
-                return TextUtils.renderTable(modelBuilder.build());
-            }catch(Exception error){
-                return error.getMessage();
-            }
-        }
-    }
-
-    @ShellMethod(key = "delete-comment", value = "Delete your comment")
-    public String deleteComment(
-        @ShellOption(value = "id", help = "The id of the comment you wish to delete", defaultValue = "") String id
-    ) {
-        if(!EnvUtils.doesKeyExist(EnvConstants.JWT_TOKEN)){
-            return "You are not logged, please login!";
-        } else if(id.equals("")){
-            return "the id must be specified like: \"delete-comment --id {id_value}\"";
-        } else{
-            try{
-
+            try {
                 CommentsServices.deleteComment(id);
-                return String.format("Your comment to id %s has been deleted", id); 
-            }catch(Exception error){
-                return error.getMessage();
+                return String.format("Your comment with id %s has been deleted", id);
+            } catch (Exception e) {
+                return "Error deleting comment: " + e.getMessage();
             }
         }
+    }
+
+    private TableModelBuilder<String> buildReplyTable(JsonNode commentNode) {
+        TableModelBuilder<String> modelBuilder = new TableModelBuilder<>();
+        modelBuilder.addRow().addValue("ID").addValue("Content")
+            .addValue("GID").addValue("User").addValue("Email")
+            .addValue("Created At").addValue("Upvotes").addValue("ThreadId");
+        if (commentNode.isArray()) {
+            for (JsonNode comment : commentNode) {
+                JsonNode userNode = comment.path("user");
+                modelBuilder.addRow()
+                    .addValue(comment.path("id").asText("N/A"))
+                    .addValue(comment.path("content").asText("N/A"))
+                    .addValue(userNode.path("id").asText("N/A"))
+                    .addValue(userNode.path("username").asText("N/A"))
+                    .addValue(userNode.path("email").asText("N/A"))
+                    .addValue(comment.path("createdAt").asText("N/A"))
+                    .addValue(comment.path("commentUpvotesCount").asText("N/A"))
+                    .addValue(comment.path("threadId").asText("N/A"));
+            }
+        } else {
+            JsonNode userNode = commentNode.path("user");
+            modelBuilder.addRow()
+                        .addValue(commentNode.path("id").asText("N/A"))
+                        .addValue(commentNode.path("content").asText("N/A"))
+                        .addValue(userNode.path("id").asText("N/A"))
+                        .addValue(userNode.path("username").asText("N/A"))
+                        .addValue(userNode.path("email").asText("N/A"))
+                        .addValue(commentNode.path("createdAt").asText("N/A"))
+                        .addValue(commentNode.path("commentUpvotesCount").asText("N/A"))
+                        .addValue(commentNode.path("threadId").asText("N/A"));
+        }
+        return modelBuilder;
     }
 }
